5760aedbe9c2b45d4cba74c34ae6457ac3471afd
Arif Ansari <arifansari90467@gmail.com>
2025-03-31 06:25:34 +0530

    updated postController

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -93,3 +93,128 @@ export const getUserPost  = async (req, res)=>{
         console.log(error);
     }
 }
+
+export const likePost = async (req, res) => {
+    try {
+        const likerId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({ message:'Post not found', success:false});
+
+        //like logic started
+        await post.updateOne({$addToSet:{likes:likerId}}); //used addtoset instead of push query buz we don't want repeatation
+        await post.save();
+
+        //implement socket.io for real time notification
+        //pending
+
+        return res.status(200).json({ message:'Post liked', success:true});
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const dislikePost = async (req, res) => {
+    try {
+        const likerId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({ message:'Post not found', success:false});
+
+        //dislike logic started
+        await post.updateOne({$pull:{likes:likerId}}); //used addtoset instead of push query buz we don't want repeatation
+        await post.save();
+
+        //implement socket.io for real time notification
+        //pending
+
+        return res.status(200).json({ message:'Post disliked', success:true});
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const addComment = async (req, res) => {
+    try {
+        const commneterId = req.id;
+        const postId = req.params.id;
+
+        const {text} = req.body;
+        const post = await Post.findById(postId);
+        if(!text) return res.status(404).json({ message:'text is required', success:false});
+
+        //dislike logic started
+        const comment = await Comment.create({
+            text,
+            author:commneterId,
+            post:postId
+        }).popolate({
+            path: 'author',
+            select:"username, profilePicture"
+        });
+
+        post.comments.push(comment._id);
+        await post.save();  
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = async (req, res) => {
+    try {
+        const authorId = req.id;
+        const postId = req.params.id;
+
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({ message:'Post not found', success:false});
+
+        //check if the logged-in user is the owner of the post or not
+        if(post.author.toString() != authorId) return res.status(403).json({ message:'Unauthorized'});
+
+        //dlt post
+        await Post.findByIdAndDelete(postId);
+
+        //remove the post id from the user's post
+        let user = await User.findById(authorId);
+        user.posts = user.posts.filter(id => id.toString()!=postId);
+        await user.save();
+
+        //dlt associated comments
+        await Comment.deleteMany({post:postId});
+
+        return res.status(200).json({
+            success:true,
+            message:'Post deleted'
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const bookmarkPost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const authorId = req.id;
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({ message:'Post not found', success:false});
+
+        const user = await user.findById(authorId);
+        if(user.bookmarks.includes(post._id)){
+            //already bookmarked -> remove from the bookmark
+            await user.updateOne({$pull:{bookmarks:post._id}});
+            await user.save();
+            return res.status(200).json({ type:'unsaved', message:'Post removed from bookmark', success:true});
+        }
+        else{
+            //add the post to the bookmark
+            await user.updateOne({$addToSet:{bookmarks:post._id}});
+            await user.save();
+            return res.status(200).json({ type:'saved', message:'Post bookmarked', success:true});
+        }
+       
+    } catch (error) {
+        console.log(error);
+    }
+}
--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -19,7 +19,7 @@ const postSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
     }],
-    comment:[{
+    comments:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Comment'
     }]
