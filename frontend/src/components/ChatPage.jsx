e8832b5ca48247ba1ebf4bf206a8350a5bc30dc0
Arif Ansari <arifansari90467@gmail.com>
2025-05-16 16:54:26 +0530

    created chatpage

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -78,8 +78,10 @@ const ChatPage = () => {
                         </div>
                         <Messages selectedUser={selectedUser} />
                         <div className='flex items-center p-4 border-t border-t-gray-300'>
-                            <Input value={textMessage} onChange={(e) => setTextMessage(e.target.value)} type="text" className='flex-1 mr-2 focus-visible:ring-transparent' placeholder="Messages..." />
-                            <Button onClick={() => sendMessageHandler(selectedUser?._id)}>Send</Button>
+                          {/* value={textMessage} onChange={(e) => setTextMessage(e.target.value)} */}
+                            <Input  type="text" className='flex-1 mr-2 focus-visible:ring-transparent' placeholder="Messages..." />
+                            {/* onClick={() => sendMessageHandler(selectedUser?._id)} */}
+                            <Button >Send</Button>
                         </div>
                     </section>
                 ) : (
--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -49,6 +49,8 @@ const LeftSidebar = () => {
       navigate(`/profile/${user?._id}`);
     } else if (textType === "Home") {
       navigate("/");
+    } else if (textType === "Messages") {
+      navigate("/chat");
     }
   };
 
