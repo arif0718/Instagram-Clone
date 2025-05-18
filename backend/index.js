15770e9922e3438fecac80971fda2f3a628edfbc
Arif Ansari <arifansari90467@gmail.com>
2025-05-19 00:57:22 +0530

    updated whole project

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,13 @@ import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config({});
 
 const PORT = process.env.PORT||3000;
+
+const __dirname = path.resolve();
 
 app.get("/", (req,res)=>{
     return res.status(200).json({
@@ -32,7 +35,13 @@ app.use(cors(corsOptions));
 //routes
 app.use("/api/v1/user", userRoute);                //"http://localhost:8000/api/v1/user"
 app.use("/api/v1/post", postRoute);  
-app.use("/api/v1/message", messageRoute);  
+app.use("/api/v1/message", messageRoute);
+
+//connecting backend+frontend for production level
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, ()=>{
     connectDB();
